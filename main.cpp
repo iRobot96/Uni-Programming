94d1d3e7bd8b53f2c7de52820c68c62b4197c349
George Burton <s15115202@cc1308t2-dm2615.students.uce.ac.uk>
2016-11-23 15:10:31 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,79 @@
+//
+//  main.cpp
+//  clas
+//
+//  Created by George Burton on 16/11/2016.
+//  Copyright © 2016 George Burton. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+class Animal
+{
+public:
+    void SetNumLegs(int x){Numlegs = x;};
+    int GetNumLegs(){return Numlegs;};
+    void setHairColour (string x) {HairColour = x;};
+    string GetHairColour(){return HairColour;};
+    
+    virtual void doANoise()
+    {
+        std::cout << "NOISE!!!!\n";
+    }
+    
+protected: int Numlegs;
+           string HairColour;
+
+};
+
+
+
+class Gorilla : public Animal
+{
+    Gorilla(){
+        Numlegs = 2;
+        HairColour = "Black";
+    }
+    
+    void doANoise() override
+    {
+        std::cout << "OOOOOOOO!\n";
+    }
+    
+};
+
+        class George : public Animal
+        {
+        public: George(){
+                Numlegs = 2;
+                HairColour ="Unpleasant brown";
+                
+            }
+         void doANoise() override
+            {
+                cout << "meh" <<endl;
+                
+            }
+
+        };
+
+
+
+
+int main() {
+    George g;
+    g.GetNumLegs();
+    g.doANoise();
+    
+    
+    
+    
+
+    
+    
+    
+    
+}
